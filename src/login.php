6be8d704de2d3595845b5ac11cfb9dce9bd49d5d
Jaydeep Rangani <ranganijaydeep@yahoo.in>
2020-04-19 19:32:29 +0100

    sign bootstrap symbols

--- src/login.php
+++ src/login.php
@@ -1,4 +1,32 @@
+<?php
+   include("config.php");
+   session_start();
 
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form
+
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+     $active = $row['active'];
+
+     $count = mysqli_num_rows($result);
+
+      If result matched $myusername and $mypassword, table row must be 1 row
+
+ if($count == 1) {
+        session_register("myusername");
+       $_SESSION['login_user'] = $myusername;
+
+       header("location: welcome.php");
+  }else {
+    echo $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
 <html>
 
    <head>
@@ -46,34 +74,6 @@
          </div>
 
       </div>
-      <?php
-         include("config.php");
-         session_start();
 
-         if($_SERVER["REQUEST_METHOD"] == "POST") {
-            // username and password sent from form
-
-            $myusername = mysqli_real_escape_string($db,$_POST['username']);
-            $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-
-            $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
-          //  $result = mysqli_query($db,$sql);
-          //  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-          //  $active = $row['active'];
-
-          //  $count = mysqli_num_rows($result);
-
-            // If result matched $myusername and $mypassword, table row must be 1 row
-
-      //      if($count == 1) {
-          //     session_register("myusername");
-          //     $_SESSION['login_user'] = $myusername;
-
-          //     header("location: welcome.php");
-        //    }else {
-        //    echo $error = "Your Login Name or Password is invalid";
-          //  }
-         }
-      ?>
    </body>
 </html>
--- src/session.php
+++ src/session.php
@@ -1,15 +1,15 @@
 <?php
    include('config.php');
    session_start();
-   
+
    $user_check = $_SESSION['login_user'];
-   
+
    $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
-   
+
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-   
+
    $login_session = $row['username'];
-   
+
    if(!isset($_SESSION['login_user'])){
       header("location:login.php");
       die();
@@ -17,13 +17,13 @@
 ?>
 <html>
    <head>
-      
+
       <style>
          span {
             color: green;
          }
       </style>
-      
+
       <script>
          function showHint(str) {
             if (str.length == 0) {
@@ -31,7 +31,7 @@
                return;
             }else {
                var xmlhttp = new XMLHttpRequest();
-					
+
                xmlhttp.onreadystatechange = function() {
                   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                      document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
@@ -42,17 +42,17 @@
             }
          }
       </script>
-      
+
    </head>
    <body>
-      
+
       <p><b>Search your favourite tutorials:</b></p>
-      
+
       <form>
          <input type = "text" onkeyup = "showHint(this.value)">
       </form>
-      
+
       <p>Entered Course name: <span id="txtHint"></span></p>
-   
+
    </body>
 </html>
