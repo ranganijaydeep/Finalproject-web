f750d1bf05141693b7571f7ed1f83df66f921211
Jaydeep Rangani <ranganijaydeep@yahoo.in>
2020-05-12 12:12:20 +0100

    node added

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+const mongodb = require('mongodb');
 
 let handleRequest = (request, response) => {
     response.writeHead(200, {
@@ -15,9 +16,29 @@ let handleRequest = (request, response) => {
         response.end();
 
     });
-
-
-
 };
+async function db_setup() {
+    const link = "mongodb+srv://Kura:gura2010@cluster-mealboxapp-qe3ie.mongodb.net/test?retryWrites=true&w=majority"
+    const DB_connection = new mongodb(link);
+    try {
+        await DB_connection.connect();
+
+        await listDatabases(DB_connection);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+db_setup().catch(console.error);
+
+async function listDatabases(DB_connection) {
+    databasesList = await DB_connection.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
 
 http.createServer(handleRequest).listen(27017);
