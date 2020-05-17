7dc036c6e72cd9010c3603478359d0c05f61035d
Jaydeep Rangani <ranganijaydeep@yahoo.in>
2020-05-17 15:17:25 +0100

    update before heroku

--- database connection/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.export = {
-    mongourl: "mongodb+srv://Kura:gura2010@cluster-mealboxapp-qe3ie.mongodb.net/test?retryWrites=true&w=majority"
-}
--- models/crew.js
+++ models/crew.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const usersSchema = Schema({
-    username: String,
-    password: String
+    Firstname: String,
+    Lastname: String,
+    Email: String,
+    Password: String
 
 });
 
